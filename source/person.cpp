40c45802699a392f325ec6b885deee25aff95975
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2020-05-27 10:38:30 +0200

    fix of exlpaination

--- source/person.cpp
+++ source/person.cpp
@@ -20,8 +20,8 @@ void Person::quitMovie(const string& m_name)
 	for (sp_Movie movie : portfolio)
 		if (movie->getTitle() == m_name) // movie is removed from portfolio via removeMovie(), called by the movie that is being removed
 		{
-			movie->fire(std::make_shared<Person>(*this)); //!ISSUE here I need to pass a shared pointer to the person that is calling the method
-		} //!ISSUE this probably works though: as the movie checks the recieved pointer with what it has, so despite creating a new pointer and immiedately deleting it, the movie manages to compare it to what it has
+			movie->fire(std::make_shared<Person>(*this)); //!ISSUE here I need to pass a shared pointer to the person that is calling the method, so Movie can compare it with its own pointer to this Person (and do stuff from Movie::fire() )
+		} //!ISSUE again, the new pointer immiedately dies after leaving this method (Person::quitMovie() ), so we have problems.
 }
 
 void Person::movieAdd(sp_Movie movie)
