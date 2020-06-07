039e1a7581d4cd872dbd7a8e90ecc810ca4c553f
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2020-06-07 20:45:41 +0200

    renaming stuff + ineractive mode start

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+/* Preliminary Project : Interactive Mode
+	Gabriel Skowron-Rodriguez */
+
+#include <iostream>
+#include <sstream> // to test returned "ostream"s
+#include "movie.hpp"
+
+int main()
+{
+	std::cout << "Hello worldXD";
+	return 0;
+}
